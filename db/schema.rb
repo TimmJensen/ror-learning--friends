b09ff5b32c710a2694fa5d6792e8cb849141e81b
Timm Jensen <timm.m.jensen@gmail.com>
2024-02-11 22:30:54 +0100

    Added friends CRUD

---
+++ db/schema.rb
@@ -0,0 +1,12 @@
+ActiveRecord::Schema[7.1].define(version: 2024_02_11_202146) do
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "github_handle"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
